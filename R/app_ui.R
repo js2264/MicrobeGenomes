a0bd151adac93d221387b2579927ce83e3fe2828
js2264 <jacquesserizay@gmail.com>
2023-12-26 08:46:23 +0100

    fix: tab spaces

--- R/app_ui.R
+++ R/app_ui.R
@@ -5,14 +5,14 @@
 #' @import shiny
 #' @noRd
 app_ui <- function(request) {
-  tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # Your application UI logic
-    fluidPage(
-      h1("MicrobeGenomes")
+    tagList(
+        # Leave this function for adding external resources
+        golem_add_external_resources(),
+        # Your application UI logic
+        fluidPage(
+            h1("MicrobeGenomes")
+        )
     )
-  )
 }
 
 #' Add external Resources to the Application
@@ -24,18 +24,18 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
-
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "MicrobeGenomes"
+    add_resource_path(
+        "www",
+        app_sys("app/www")
     )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert()
-  )
+
+    tags$head(
+        favicon(),
+        bundle_resources(
+            path = app_sys("app/www"),
+            app_title = "MicrobeGenomes"
+        )
+        # Add here other external resources
+        # for example, you can add shinyalert::useShinyalert()
+    )
 }

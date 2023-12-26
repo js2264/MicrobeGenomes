ca0dd7e11494a59128fc08b2bf0f534ce2ab7d20
js2264 <jacquesserizay@gmail.com>
2023-12-27 00:59:48 +0100

    feat: front-end

--- R/app_server.R
+++ R/app_server.R
@@ -5,5 +5,27 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-    # Your application server logic
+    
+    species <- reactive ({ input$species })
+
+    output$metrics <- DT::renderDT({
+        shinipsum::random_DT(5, 5)
+    })
+    output$structural_features <- DT::renderDT({
+        shinipsum::random_DT(5, 5)
+    })
+    output$data <- renderText({
+        shinipsum::random_text(nwords = 50)
+    })
+
+
+    output$map <- renderPlot({
+        shinipsum::random_ggplot()
+    })
+    output$ps <- renderPlot({
+        shinipsum::random_ggplot()
+    })
+    output$aggregated <- renderPlot({
+        shinipsum::random_ggplot()
+    })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -6,11 +6,74 @@
 #' @noRd
 app_ui <- function(request) {
     tagList(
-        # Leave this function for adding external resources
+
         golem_add_external_resources(),
-        # Your application UI logic
-        fluidPage(
-            h1("MicrobeGenomes")
+
+        # fluidPage(
+        #     titlePanel("Microbe Genomes Community"),
+        #     splitLayout(
+        #         cellWidths = c("33%", "66%"),
+        #         sliderInput(
+        #             "bins", label = "Number of bins:",
+        #             min = 1, value = 30, max = 50
+        #         ) |> wellPanel(),
+        #         splitLayout(
+        #             cellWidths = c("50%", "50%"),
+        #             plotOutput("plot"), 
+        #             textOutput("text")
+        #         ) |> wellPanel()
+        #     )
+        # )
+
+        shinydashboardPlus::dashboardPage(
+            header = shinydashboardPlus::dashboardHeader(
+                title = "Microbe Genomes Community", 
+                titleWidth = '20%'
+            ),
+            sidebar = shinydashboardPlus::dashboardSidebar(
+                width = "20%", 
+                selectizeInput(
+                    "species", 
+                    "Pick a microbe: ", 
+                    choices = shinipsum::lorem_words[1:1000], 
+                    selected = 1, 
+                    multiple = FALSE
+                ) 
+            ),
+            body = shinydashboard::dashboardBody(
+                column(12,
+                    fluidRow(
+                        column(4,
+                            h2("Genome reference metrics"), 
+                            dataTableOutput("metrics"),
+                        ), 
+                        column(4,
+                            h2("Contact matrix"), 
+                            plotOutput("map"),
+                        ),
+                        column(4,
+                            h2("Genome structural features"), 
+                            dataTableOutput("structural_features"),
+                        )
+                    ), 
+                    br(),
+                    br(),
+                    fluidRow(
+                        column(4,
+                            h2("Data access"),
+                            textOutput("data"),
+                        ), 
+                        column(4,
+                            h2("P(s) representation"), 
+                            plotOutput("ps"),
+                        ),
+                        column(4,
+                            h2("Aggregated plot"), 
+                            plotOutput("aggregated"),
+                        )
+                    )
+                )
+            )
         )
     )
 }
